c49893047a6747e4f1820571aacd1ac2cbfea181
MrPiddlesticks <44594756+MrPiddlesticks@users.noreply.github.com>
2021-12-10 22:42:49 -0500

    Create dasher.cpp

---
+++ dasher.cpp
@@ -0,0 +1,63 @@
+#include "raylib.h"
+
+int main()
+{
+    // window dimensions
+    const int windowWidth {512};
+    const int windowHeight {380};
+    // init window
+    InitWindow(windowWidth, windowHeight, "Dapper-Dasher");
+
+    // acceleration due to gravity (pixels/frame)/frame
+    const int gravity{1};
+
+    // rectangle dimenstions
+    const int width{50};
+    const int height{80};
+
+    // is the rectangle in the air
+    bool isInAir{};
+    //jump velocity
+    const int jumpVel{-22};
+
+    int posY{windowHeight - height};
+    int velocity{0};
+
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose())
+    {
+        // start drawing
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // perform ground check
+        if (posY >= windowHeight - height)
+        {
+            // rectangle is on the ground
+            velocity=0;
+            isInAir=false;
+        }
+        else
+        {
+            // rectangle is in the air
+            velocity += gravity;
+            isInAir = true;
+        }
+        // jump check
+        if(IsKeyPressed(KEY_SPACE) && !isInAir)
+        {
+            velocity += jumpVel;
+        }
+        
+        
+        // update postion
+        posY += velocity;
+
+        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
+
+        // stop drawing
+        EndDrawing();
+    }
+    CloseWindow();
+}
